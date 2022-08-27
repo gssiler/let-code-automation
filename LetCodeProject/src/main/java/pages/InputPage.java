4702cdc423b899709c6704a5a38fa7b038d4d1eb
gssiler <33408643+gssiler@users.noreply.github.com>
2022-08-27 18:47:02 -0400

    Completed tests to clear text from field and confirm field is disabled.

--- LetCodeProject/src/main/java/pages/InputPage.java
+++ LetCodeProject/src/main/java/pages/InputPage.java
@@ -17,6 +17,14 @@ public class InputPage extends PageObjectBase {
 	@FindBy(xpath = "//input[@id='getMe']")
 	private WebElement returnValue;
 
+	@CacheLookup
+	@FindBy(xpath = "//input[@id='clearMe']")
+	private WebElement clearField;
+
+	@CacheLookup
+	@FindBy(xpath = "//input[@id='noEdit']")
+	private WebElement disabledField;
+
 	protected InputPage(WebDriver driver, String baseUrl) {
 
 		super(driver, baseUrl);
@@ -35,6 +43,29 @@ public class InputPage extends PageObjectBase {
 		System.out.println(value);
 
 		return value;
+	}
+
+	public String clearField() {
+
+		elementControl.clear(clearField);
+		String value = clearField.getAttribute("value");
+		System.out.println(value);
+
+		return value;
+	}
+
+	public InputPage confirmFieldIsDisabled() {
+
+		Boolean isEnabled = elementControl.isEnabled(disabledField);
+
+		if (isEnabled) {
+			System.out.println("Element is enabled");
+		}
+		else {
+			System.out.println("Element is not enabled");
+		}
+
+		return this;
 	}
 
 
--- LetCodeProject/src/test/java/tests/InputTests.java
+++ LetCodeProject/src/test/java/tests/InputTests.java
@@ -34,4 +34,27 @@ public class InputTests extends LetCodeTestBase{
 		assertEquals(actualValue, value, "The expeceted value and the actual value do not match.");
 
 	}
+
+	@Test
+	public void canClearTextFromField() {
+
+		String expectedText = "";
+
+		String actualText = new HomePage(getDriver(), getBaseUrl())
+				.navigate()
+				.clickInputButton()
+				.clearField();
+
+		assertEquals(actualText, expectedText);
+
+	}
+
+	@Test
+	public void canConfirmEditFieldIsDisabled() {
+
+		new HomePage(getDriver(), getBaseUrl())
+		.navigate()
+		.clickInputButton()
+		.confirmFieldIsDisabled();
+	}
 }
